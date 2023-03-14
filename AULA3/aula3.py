7bca81630b02e9bfb6aee03ec1865c732926a764
diogolameira <diogolameira18@gmail.com>
2023-03-14 17:43:11 +0000

    Aula3

--- AULA3/aula3.py
+++ AULA3/aula3.py
@@ -2,11 +2,12 @@ import re
 
 file = open('dicionario_medico.txt', encoding='utf8')
 text = file.read()
-text = re.sub(r'\f', '', text)
+text = re.sub(r'\f', '\n', text)
+text = re.sub(r'(\n\n.+)\n\n', 'r\1', text)
 entries = re.findall(r'\n\n(.+)((?:\n.+)+)', text)
 #print(entries[:3])
 new_entries = [(designation, description.strip()) for designation, description in entries]
-print(new_entries[:3])
+#print(new_entries[:3])
 '''
 new_entries = []
 for designation, description in entries:
@@ -21,16 +22,44 @@ html = open('dicionario_medico.html', 'w', encoding='utf8')
 header = '''<html>
 <head>
 <meta charset='utf-8'/>
-</head>
-<body>
+<style>
+table {
+  font-family: helvetica;
+  border-collapse: collapse;
+  width: 100%;
+}
 
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<title>Dicionário Médico</title>
+<body>
+<h1 style="text-align:center; font-family:helvetica;">Dicionário Médico</h1>
+<br>
+<table>
+<tr>
+<th>Termo</th>
+<th>Designação</th>
+</tr>
 '''
 body = ''
 for designation, description in new_entries:
-    body += '<b>' + designation + '</b>'
-    body += '<p>' + description + '</p>' + '<hr>'
+    body += '<tr>'
+    body += '<td>' + designation + '</td>'
+    body += '<td>' + description + '</td>'
+    body += '</tr>'
 
-footer = ''' </body>
+footer = '''
+</table>
+</body>
 </html>
 '''
 html.write(header + body + footer)
